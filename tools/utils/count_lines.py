704d62c6286cfd5a781c560c25848f1f609b0836
utshowmh <utshowmh@gmail.com>
2022-11-06 16:42:31 +0600

    updated utils

--- tools/utils/count_lines.py
+++ tools/utils/count_lines.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 
 from os import walk
-
-ROOT = "./src"
-
-line_count = 0
-source_files = []
+from fire import Fire
 
 
-def find_files():
-    for path in walk(ROOT):
+def count_lines(root_path: str = "."):
+    line_count = 0
+    source_files = []
+
+    for path in walk(root_path):
         if len(path) != 3:
             raise Exception("Underflow!!!")
         for file in path[2]:
             source_files.append(f"{path[0]}/{file}")
 
-
-if __name__ == "__main__":
-    find_files()
     for file in source_files:
         with open(file, 'r') as f:
             lines = len(f.read().split('\n'))
             line_count += lines
             print(f"found {lines} lines in {file}")
-    print("---------------------------------------")
+
+    print("-"*100)
     print(f"found {line_count} lines in {len(source_files)} files.")
+
+
+if __name__ == "__main__":
+    Fire(count_lines)
